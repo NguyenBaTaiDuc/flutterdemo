71e716735d76153251173f7d2782e9e04db32197
ducng <ducnguyenbatai@gmail.com>
2024-11-02 12:44:13 +0700

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterdemo/Student.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // inplement object
+  final List<Student> student = [];
+  // fuction add
+  void _addstudent (String name){
+    student.add(Student(name: name));
+  }
+  //function remove
+  void _removestudent (int id){
+    setState(() {
+      student.removeAt(id);
+    });
+  }
+  void _toggle (int id){
+    setState(() {
+      student[id].isDone = !student[id].isDone;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView'),
+      ),
+      //create ListView
+      body: ListView.builder(
+          itemCount: student.length,
+          itemBuilder: (context,index){
+            return ListTile(
+              title: Text(
+                student[index].name,
+                style: TextStyle(
+                  decoration: student[index].isDone? TextDecoration.lineThrough: TextDecoration.none,
+                ),
+              ),
+              //button delete
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () => {
+                  _toggle(index),
+                },
+              ),
+
+            );
+          }),
+      //button add
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/Student.dart
@@ -0,0 +1,10 @@
+
+//create Ojbect
+class Student {
+ late int id;
+ late String name;
+  late String description;
+  bool isDone;
+
+ Student( {required this.name,this.isDone = false});
+}
